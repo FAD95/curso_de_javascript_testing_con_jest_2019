4a799ff8ef86307316988653803b8fae7c4e9ecd
Fadith Escorcia <feescorciam@protonmail.com>
2020-08-09 01:46:50 -0500

    .toBeGreaterThan

---
+++ __test__/numbers.test.js
@@ -0,0 +1,8 @@
+import { numbers } from '../numbers'
+
+describe('Compare numbers', () => {
+    test('should be grater than', () => {
+        expect(numbers(2,2)).toBeGreaterThan(3)
+    })
+    
+})
