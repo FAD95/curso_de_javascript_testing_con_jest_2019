8175d0e27044ef75197c5c96a9cdcbfc08ccb403
Fadith Escorcia <feescorciam@protonmail.com>
2020-08-09 01:59:19 -0500

    toBeLessThanOrEqual

--- __test__/numbers.test.js
+++ __test__/numbers.test.js
@@ -4,10 +4,14 @@ describe('Compare numbers', () => {
   test('should be grater than', () => {
     expect(numbers(2, 2)).toBeGreaterThan(3)
   })
-  test('should be grater than', () => {
+  test('should be grater than or equal', () => {
     expect(numbers(2, 2)).toBeGreaterThanOrEqual(4)
   })
-  test('should be grater than', () => {
+  test('should be less than', () => {
     expect(numbers(2, 2)).toBeLessThan(5)
   })
+  test('should be less than or equal', () => {
+    expect(numbers(2, 2)).toBeLessThanOrEqual(4)
+  })
+ 
 })
