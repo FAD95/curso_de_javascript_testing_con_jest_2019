be3ac5e6ded4eedf4e3b767ea7f79d21a1d178a1
Fadith Escorcia <feescorciam@protonmail.com>
2020-08-09 01:22:03 -0500

    Todas las funciones de math.js testeadas

--- __test__/maths.test.js
+++ __test__/maths.test.js
@@ -4,4 +4,13 @@ describe('Math operations', () => {
     test('Sum test', () => {
         expect(sum(1,1)).toBe(2)
     })
+    test('Substract test', () => {
+      expect(subtract(1, 1)).toBe(0)
+    })
+    test('Multiply test', () => {
+      expect(multiply(2, 5)).toBe(10)
+    })
+    test('Divide test', () => {
+      expect(divide(10, 2)).toBe(5)
+    })
 })
