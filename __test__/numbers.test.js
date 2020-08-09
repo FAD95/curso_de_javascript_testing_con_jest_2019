b72d6958191c228ce99173488a65845af0d1ed60
Fadith Escorcia <feescorciam@protonmail.com>
2020-08-09 01:52:05 -0500

    toBeLessThan

--- __test__/numbers.test.js
+++ __test__/numbers.test.js
@@ -1,10 +1,13 @@
 import { numbers } from '../numbers'
 
 describe('Compare numbers', () => {
-    test('should be grater than', () => {
-        expect(numbers(2,2)).toBeGreaterThan(3)
-    })
-     test('should be grater than', () => {
-       expect(numbers(2, 2)).toBeGreaterThanOrEqual(4)
-     })
+  test('should be grater than', () => {
+    expect(numbers(2, 2)).toBeGreaterThan(3)
+  })
+  test('should be grater than', () => {
+    expect(numbers(2, 2)).toBeGreaterThanOrEqual(4)
+  })
+  test('should be grater than', () => {
+    expect(numbers(2, 2)).toBeLessThan(5)
+  })
 })
